f96893c2e218a1a990d36f832c7a6df7a4f33323
Ram Boddula <sairam.boddula@gmail.com>
2019-06-21 23:28:10 -0500

    Initial commit

---
+++ src/main/java/com/flowerpot/ridespare/RidespareApplication.java
@@ -0,0 +1,13 @@
+package com.flowerpot.ridespare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidespareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidespareApplication.class, args);
+	}
+
+}
